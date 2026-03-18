89afa52524d038c86f0b9777303ce7aeb6c6d008
Christopher Kapic <christopherkapic@gmail.com>
2026-03-18 16:08:07 +0000

    Fix

--- apps/web/src/routes/_auth/agents/$slug.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Outlet, createFileRoute } from "@tanstack/react-router";
-export const Route = createFileRoute("/_auth/agents/$slug")({
-    component: AgentSlugLayout,
-});
-function AgentSlugLayout() {
-    return _jsx(Outlet, {});
-}
